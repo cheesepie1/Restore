424faa91d1b59f4df24a5fe837c71d312ec7e68e
Joe <54168514lee@gmail.com>
2025-02-03 15:42:17 +1100

    initial commit

---
+++ API/Program.cs
@@ -0,0 +1,18 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddDbContext<StoreContext>(opt =>{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnections"));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.MapControllers();
+
+DbInitializer.InitDb(app);
+app.Run();
